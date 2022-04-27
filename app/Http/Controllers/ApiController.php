b2932a5782323bf138cb3592bb1f60198cb12896
Richard McLeod <rmcleod79@gmail.com>
2022-04-27 12:35:05 +0100

    Model and migration for storing data

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Exceptions\TranscriptionException;
 use App\Exceptions\ValidationException;
 use App\Http\Validators\UploadValidator;
+use App\Models\Transcription;
 use App\Services\GoogleClient;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -18,21 +19,33 @@ class ApiController extends Controller
 
     public function upload(Request $request): Response
     {
+        $time = date('Y-m-d H:i:s');
         try {
             $this->validator->validateUpload($request->json('content'));
         } catch (ValidationException $e) {
             return response($e->getMessage(), 400);
         }
 
-        Storage::put('test.flac', base64_decode($request->json('content')));
+        $filename = $request->json('filename') . $time . '.flac';
+        Storage::put($filename, base64_decode($request->json('content')));
+        $transcription = Transcription::create([
+            'filename' => $filename,
+            'submitted' => $time,
+            'status' => 'Started',
+        ]);
 
         try {
             $client = new GoogleClient();
-            $transcription = $client->transcribe(Storage::path('test.flac'));
+            $transcription->transcription = $client->transcribe(Storage::path('test.flac'));
+            $transcription->status = 'Complete';
+            $transcription->save();
         } catch (TranscriptionException $e) {
+            $transcription->status = 'Failed';
+            $transcription->error = $e->getMessage();
+            $transcription->save();
             return response($e->getMessage(), 502);
         }
 
-        return response($transcription);
+        return response($transcription->id, 203);
     }
 }
--- database/migrations/2022_04_27_111817_create_transcriptions_table.php
+++ database/migrations/2022_04_27_111817_create_transcriptions_table.php
@@ -13,13 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transcriptions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('filename');
+            $table->longText('transcription')->nullable();
+            $table->dateTime('submitted');
+            $table->enum('status', [
+                'Complete',
+                'Failed',
+                'Started',
+            ]);
+            $table->text('error')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transcriptions');
     }
 };
